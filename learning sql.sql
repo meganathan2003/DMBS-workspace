f8f143191b5461546c45c5b4204b0a13c9fbd07e
Meganathan <meganathan212003@gmail.com>
2023-03-18 13:05:28 +0530

    add DBMS files

---
+++ learning sql.sql
@@ -0,0 +1,7 @@
+/*
+DATATYPES IN SQL 
+CHAR(10) -- This is the fixed value allocated the more memory , whne we use char EXAMPLE - airport
+VARCHAR(100) -- This is something like reduce the memory and this is not a fixed
+/*
+
+show database;
---
+++ student dbms.sql
@@ -0,0 +1,1 @@
+SELECT * FROM meganathandb.student;
